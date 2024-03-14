025a208cbc33320f62eeef6f807503dedde68c8a
dominick253 <dominick253@gmail.com>
2024-03-14 13:20:03 -0500

    Feat: .Reverse() .Sort()

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1266,3 +1266,95 @@
 
 
 
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+
+// Console.WriteLine("Sorted...");
+
+// Array.Sort(pallets);
+
+// for (int i = 0; i < pallets.Length; i++)
+// {
+//     Console.WriteLine($"-- {pallets[i]}");
+// }
+
+// Console.WriteLine("Reversed...");
+
+// Array.Reverse(pallets);
+
+// foreach (string pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+
+
+
+// Console.WriteLine($"Before: {pallets[0]}");
+// Array.Clear(pallets, 0, 2);
+// if (pallets[0] != null)
+//     Console.WriteLine($"after: {pallets[0].ToLower()}");
+
+// Console.WriteLine($"Clearing 2 .. count: {pallets.Length}");
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+
+// Console.WriteLine("");
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"Clearing 2 .. count: {pallets.Length}");
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+
+// Array.Resize(ref pallets, 6);
+
+// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+// pallets[4] = "C01";
+// pallets[5] = "C02";
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+
+// Array.Resize(ref pallets, 3);
+
+// Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+
+
+
+
+
+
+
+
+
+
